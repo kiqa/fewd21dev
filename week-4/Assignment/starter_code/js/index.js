a32be809b1482f640cab4fc1117fde1f0e0ff322
kiqa <kellyalanya@hotmail.com>
2016-02-25 23:34:00 -0500

    assignment KW

--- week-4/Assignment/starter_code/js/index.js
+++ week-4/Assignment/starter_code/js/index.js
@@ -1,13 +1,14 @@
 $( document ).ready(function() {
-	
+	// KW= this part is for the articles buttons: read more and readless hide. one of my issues was the space in each one because Jquery didn't respond when I put the space. so I decided to put dot same as class and it works!! :)//
 	$(".readless.hide1").click(disapearText1);
 	$(".readless.hide2").click(disapearText2);
 	$(".read.more1").click(showOnClick1);
 	$(".read.more2").click(showOnClick2);
+	// KW= This part is the aside. I tryed the put "learnmoretext" with event"hide" but doesn't work,so when I removed it finaly works. //
 	$(".learnmore").click(showOnClick3);
 	
 	
-	
+	// KW= when i was clicking my web page jumps up the head so I research in the web and I found two opcions "e.preventDefault();" and "return false;" and I took return false because it is easy tu remmember. I would like to know what is the difference between this two method// 
 	function showOnClick1(){
 		$("#show-this-on-click1").slideDown();
 		return false;
